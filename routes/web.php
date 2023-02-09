b5934f90673804e06ec20d8be4761b989c899cf8
Rendy I <risdwanta35@gmail.com>
2023-02-09 11:38:00 +0700

    db connection test

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dbconn', function () {
+    return view('dbconn');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
