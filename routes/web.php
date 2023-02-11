82bda2a8751115c971f92a2bec3879f8f7d31475
Rendy I <risdwanta35@gmail.com>
2023-02-11 10:14:32 +0700

    add missing sass

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/dbconn', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
