8c64c1eb699868439485e95229408ca7c34945d9
Rendy I <risdwanta35@gmail.com>
2023-03-14 13:58:59 +0700

    Remove welcome page

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,4 @@ Route::get('/dbconn', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
