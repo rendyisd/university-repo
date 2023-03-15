65b272f45d75dcff6d96526f0cf3bff4f355e955
Rendy I <risdwanta35@gmail.com>
2023-03-15 08:55:43 +0700

    login/register dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,19 +84,6 @@
 
                                 </div>
                             </li>
-                            <!-- Authentication Links -->
-                            {{-- @guest
-                                @if (Route::has('login'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                    </li>
-                                @endif
-
-                                @if (Route::has('register'))
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                    </li>
-                                @endif --}}
                             @endif
                             @else
                                 <li class="nav-item dropdown">
