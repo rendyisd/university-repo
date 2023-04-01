f7bfc100b8a2064f7319a84e2472c51a12066bc9
Rico <ricoarcane23@gmail.com>
2023-04-01 08:56:19 +0700

    add approvement and pending

---
+++ app/Http/Controllers/PendingController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PendingController extends Controller
+{
+    public function index(){
+    return view('pending');
+    }
+}
---
+++ resources/views/pending.blade.php
@@ -0,0 +1,125 @@
+@extends('layouts.app')
+
+@section('scripts')
+ @vite([
+    'resources/sass/admin.scss',
+]) 
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="title-search pb-2 mb-4">
+                <i class="fa-solid fa-book me-2"></i>
+                <span class="fw-bold">Pending page</span>
+            </div>
+        </div>
+    </div>
+    <table class="table table-striped table-bordered">
+        <tbody>
+            <tr>
+                <th scope="col" class="fw-bold">Bibliography</th>
+                <th scope="col" class="fw-bold">Content</th>
+                <th scope="col" class="fw-bold">Status</th>
+            </tr>
+            <tr>
+                <td class="col-bibliography">
+                    <a href="#" class="fw-bold">
+                        Colonization process determines species diversity via competitive quasi-exclusion
+                    </a>
+                    <p class="col-bibliography-sub">
+                        Sugimoto, Koichi; Ono, Eiichiro; Inaba, Tamaki; Tsukahara, Takehiko; Matsui, Kenji
+                    </p>
+                    <p class="col-bibliography-sub">
+                        Undergraduate Thesis
+                    </p>
+                    <p class="col-bibliography-sub">
+                        Sriwijaya University, Faculty of Computer Science, Computer System
+                    </p>
+                </td>
+                <td class="text-center">
+                    <a href="#">
+                        <i class="fa-regular fa-file-pdf" style="font-size: 60px"></i>
+                    </a>
+                </td>
+                <td class="approve text-center">
+                    Pending...
+                </td>
+            </tr>
+            <tr>
+                <td class="col-bibliography">
+                    <a href="#" class="fw-bold">
+                        Fresh-marketable tomato yields enhanced by moderate weed control and suppressed fruit dehiscence with woodchip mulching
+                    </a>
+                    <p class="col-bibliography-sub">
+                        Horimoto, Sakae; Fukuda, Kazuaki; Yoshimura, Jin; Ishida, Atsushi (2022)
+                    </p>
+                    <p class="col-bibliography-sub">
+                        Master Thesis
+                    </p>
+                    <p class="col-bibliography-sub">
+                        Sriwijaya University, Faculty of Agriculture, Agribusiness
+                    </p>
+                </td>
+                <td class="text-center">
+                    <a href="#">
+                        <i class="fa-regular fa-file-pdf" style="font-size: 60px"></i>
+                    </a>
+                </td>
+                <td class="approve text-center">
+                    Pending...
+                </td>
+            </tr>
+            <tr>
+                <td class="col-bibliography">
+                    <a href="#" class="fw-bold">
+                        Fresh-marketable tomato yields enhanced by moderate weed control and suppressed fruit dehiscence with woodchip mulching
+                    </a>
+                    <p class="col-bibliography-sub">
+                        Horimoto, Sakae; Fukuda, Kazuaki; Yoshimura, Jin; Ishida, Atsushi (2022)
+                    </p>
+                    <p class="col-bibliography-sub">
+                        Master Thesis
+                    </p>
+                    <p class="col-bibliography-sub">
+                        Sriwijaya University, Faculty of Agriculture, Agribusiness
+                    </p>
+                </td>
+                <td class="text-center">
+                    <a href="#">
+                        <i class="fa-regular fa-file-pdf" style="font-size: 60px"></i>
+                    </a>
+                </td>
+                <td class="approve text-center">
+                    Pending...
+                </td>
+            </tr>
+            <tr>
+                <td class="col-bibliography">
+                    <a href="#" class="fw-bold">
+                        Fresh-marketable tomato yields enhanced by moderate weed control and suppressed fruit dehiscence with woodchip mulching
+                    </a>
+                    <p class="col-bibliography-sub">
+                        Horimoto, Sakae; Fukuda, Kazuaki; Yoshimura, Jin; Ishida, Atsushi (2022)
+                    </p>
+                    <p class="col-bibliography-sub">
+                        Master Thesis
+                    </p>
+                    <p class="col-bibliography-sub">
+                        Sriwijaya University, Faculty of Agriculture, Agribusiness
+                    </p>
+                </td>
+                <td class="text-center">
+                    <a href="#">
+                        <i class="fa-regular fa-file-pdf" style="font-size: 60px"></i>
+                    </a>
+                </td>
+                <td class="approve text-center">
+                    Pending...
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+@endsection
